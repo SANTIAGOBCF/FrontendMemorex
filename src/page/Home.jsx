26358fcab6d0b8c87cd5115ded37910c75d9c471
cristianaquino <ycristian10@gmail.com>
2022-12-22 15:08:18 -0500

    home

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,10 +1,7 @@
 import { padding } from "@mui/system";
 import DrawerAppBar from "../components/Home/DrawerAppBar";
+import Principal from "../components/Home/Principal";
 
 export const Home = () => {
-  return (
-  < >
-    <DrawerAppBar/>
-  </>
-  )
+  return <Principal />;
 };
