8b46a21511de4cf7e113617b038c5c0167d8b755
cristianaquino <ycristian10@gmail.com>
2022-12-22 15:08:24 -0500

    profile

--- src/page/ProfileUser.jsx
+++ src/page/ProfileUser.jsx
@@ -2,7 +2,6 @@ import { Profile } from "../components/Profile/Profile";
 import { useSelector } from "react-redux";
 
 export const ProfileUser = () => {
-  const { token } = useSelector((state) => state.authSlice);
-  const { data } = useSelector((state) => state.editDataSlice);
-  return <Profile token={token} data={data} />;
+  const { token, user } = useSelector((state) => state.authSlice);
+  return <Profile token={token} data={user} />;
 };
