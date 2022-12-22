685a48dff5c7bda6088c6a1dcc39f7ae0fbfe703
cristianaquino <ycristian10@gmail.com>
2022-12-22 15:11:38 -0500

    utils - data prueba

---
+++ src/utils/data.js
@@ -0,0 +1,38 @@
+export const data = [
+  {
+    id: "3213123j21n1",
+    email: "holamundo1",
+    role: "admin",
+  },
+  {
+    id: "3213123j21n2",
+    email: "holamundo2",
+    role: "string99",
+  },
+  {
+    id: "3213123j21n3",
+    email: "holamundo3",
+    role: "string99",
+  },
+  {
+    id: "3213123j21n4",
+    email: "holamundo4",
+    role: "admin",
+  },
+  {
+    id: "3213123j21n5",
+    email: "holamundo5",
+    role: "string99",
+  },
+  {
+    id: "1ff00e34-8ea0-4064-a25e-b7f3a4b99d65",
+    created_at: "2022-12-16T02:51:47.414Z",
+    email: "string99",
+    first_name: "string99",
+    last_name: "string99",
+    last_login: null,
+    profile_image: "string99",
+    role: "string99",
+    updated_at: "2022-12-16T02:51:47.414Z",
+  },
+];
---
+++ src/utils/reports.js
@@ -0,0 +1,19 @@
+const report = [
+  {
+    id: "string11",
+    postId: "string1",
+    report: "terrorismo",
+  },
+  {
+    id: "string22",
+    postId: "string2",
+    report: "sexual",
+  },
+  {
+    id: "string44",
+    postId: "string4",
+    report: "odio",
+  },
+];
+
+export default report;
