79f65f1a2f4299487fe43313f80702fbf50bb9e2
Omar-Villarreal <64047589+OmarVD-code@users.noreply.github.com>
2022-12-24 00:22:54 -0500

    post imagen add pj

--- src/components/Personajes/Form.jsx
+++ src/components/Personajes/Form.jsx
@@ -1,15 +1,41 @@
+import { useState } from "react";
+
 import { useDispatch } from "react-redux";
 import { addPolitician } from "../../https/postPolitician";
+
+import { postsImage } from "../../https/uploadImage";
 import { useForm } from "react-hook-form";
 
 export const ModalFormComponent = () => {
+    // const [image, setImage] = useState(null);
+
     const dispatch = useDispatch();
     const token = window.sessionStorage.getItem("token").replace(/['"]+/g, '')
  
 
     const { register, formState: {errors}, handleSubmit } = new useForm();
 
+    // const onImageChange = (e) => {
+    //     if (e.target.files && e.target.files[0]) {
+    //       let img = e.target.files[0];
+    //       setImage(img);
+    //     }
+    //     console.log("hello")
+    // };
+
     const onSubmit = (data) => {
+        const valor = new FormData();
+        const filename = data.image[0].name;
+        valor.append("name", filename);
+        valor.append("file", data.image[0]);
+        postsImage(valor).then((res) => add(res, data),
+        );
+        
+       
+    }
+
+    const add = (res, data) => {
+        data.profile_image = res.URL
         addPolitician(data, token).then(
             (response) => console.log(response),
             setTimeout(() => {
@@ -17,11 +43,10 @@ export const ModalFormComponent = () => {
               }, 3000)
             )
         .catch((e) => console.log(e));
-        // console.log(window.sessionStorage.getItem("token"));
     }
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={handleSubmit(onSubmit)} encType="multipart/form-data">
             <div className="mb-3">
                 <label htmlFor="name">Nombre</label>
                 <input
@@ -62,14 +87,14 @@ export const ModalFormComponent = () => {
             </div>
 
             <div className="mb-3">
-                <label htmlFor="profile_image">Imagen</label>
+                <label htmlFor="image">Imagen</label>
                 <input
-                    name="profile_image"
+                    name="image"
                     className="form-control" 
-                    type="text" 
-                    id="profile_image"
-                    {...register("profile_image", {required: false})}
-                    aria-invalid={errors.profile_image ? "true" : "false"}
+                    type="file" 
+                    id="image"
+                    {...register("image", {required: false})}
+                    aria-invalid={errors.image ? "true" : "false"}
                 />
             </div>
             
